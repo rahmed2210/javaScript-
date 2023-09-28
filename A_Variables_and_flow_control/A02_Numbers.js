ebee248155470a722e13880e0b51eae3e7d74cc4
Rubal <rahmed2210@gmail.com>
2023-09-28 11:20:05 -0400

    Numbers

---
+++ A_Variables_and_flow_control/A02_Numbers.js
@@ -0,0 +1,42 @@
+//-----------------------  NOTES   --------------------------------------------
+console.log('\r\n..................((((( Notes ))))).......................\r\n')
+
+
+// Create a number and assign to a variable
+let age = 26
+
+// Add
+let addResult = age + 1 // 27
+
+// Subtract
+let subtractResult = age - 26.5 // -0.5
+
+// Multiply
+let multiplyResult = age * 10 // 260
+
+// Divide
+let divideResult = age / 2 // 13
+
+// Parenthesis can be used to make sure one operation happens before another
+let result = (age + 3) * 7 // 203
+
+
+
+//-----------------------  CHALLENGE   --------------------------------------------------
+console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+
+let studentScore = 18
+let maxScore = 20
+// percent
+
+//  18 / 20 * 100
+
+let percent = (studentScore / maxScore) * 100
+console.log(percent)
+
+
+let studentScore1 = 41
+let maxScore1 = 100
+
+let percent1 = (studentScore1 / maxScore1) * 100
+console.log(percent1)
