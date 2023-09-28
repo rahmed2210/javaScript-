589e57d2308ec923f1386efe8314e2a4848d5391
Rubal <rahmed2210@gmail.com>
2023-09-28 11:33:05 -0400

    Variables

---
+++ A_Variables_and_flow_control/A03_variables.js
@@ -0,0 +1,19 @@
+
+// 1. you can't define a variable more than once
+let petName = 'Belle'
+
+// to change petName to different name
+ petName = 'Spot'
+
+console.log(petName)
+
+//  2. There are rules related to variable names
+
+/*      javaScript doesn't know if you want to use the variables three or the number three so what is does is it makes
+number invalid as the first character in variables names 
+let 3 = 2
+let result = 3 + 4
+*/
+
+// 3. Variable names cannot be reserved keyword
+
