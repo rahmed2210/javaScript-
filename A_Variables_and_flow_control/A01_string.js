8a6b3cf07dde106806c510053efe5bee374aee78
Rubal <rahmed2210@gmail.com>
2023-09-28 10:13:06 -0400

    String and variables

---
+++ A_Variables_and_flow_control/A01_string.js
@@ -0,0 +1,22 @@
+console.log('\r\n..................((((( Exercise Area ))))).......................\r\n')
+
+// let  ---> is know as a reserved keyword in JavaScript
+
+let firstName = 'rubal'
+let lastName = 'ahmed'
+let fulName = firstName + ' ' + lastName
+
+console.log(fulName)  // rubal ahmed
+
+
+
+//-----------------------  CHALLENGE   ---------------------------------------------------
+console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+
+// city, country, location
+
+let city = 'Paterson'
+let country = 'United State'
+let location = city + ', ' + country
+
+console.log(location)
