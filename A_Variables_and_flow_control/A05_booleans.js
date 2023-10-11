1737679ae12c883988e5d86a6b92cb4e6f6f066b
Rubal <rahmed2210@gmail.com>
2023-10-11 10:48:24 -0400

    Booleans and Comparison Operators

---
+++ A_Variables_and_flow_control/A05_booleans.js
@@ -0,0 +1,65 @@
+//            Boolean
+
+let temp = 31
+
+//  ---->  ===  Equality Operator
+console.log('\r\n ===  Equality Operator')
+let isFreezing = temp === 31
+console.log(isFreezing)
+
+//  ---->       !==  Not Equal Operator
+console.log('\r\n !==  Not Equal Operator')
+let isFreezing1 = temp !== 31
+console.log(isFreezing1)
+
+//  ---->   <    Less Than Equality Operator
+console.log('\r\n <  Less Than Equality Operator')
+let isFreezing2 = temp < 31
+console.log(isFreezing2)
+
+//  ---->    >   Greater Than Operator
+console.log('\r\n >   Greater Than Operator')
+let isFreezing3 = temp > 31
+console.log(isFreezing3)
+
+//  ---->    <=   Less Than Or Equal to Operator
+console.log('\r\n <=   Less Than Or Equal to Operator')
+let isFreezing4 = temp > 31
+console.log(isFreezing4)
+
+//  ---->   >=   Greater Than or Equal to Operator
+console.log('\r\n >=   Greater Than or Equal to Operator')
+let isFreezing5 = temp > 31
+console.log(isFreezing4)
+
+
+//-----------------------  CHALLENGE   --------------------------------------------------
+console.log('\r\n..................((((( Challenge Area ))))).......................\r\n')
+
+//   create age set to your age
+//   Calculate is child - if they are 7 or under
+//   Calculate is senior  - if they are 65 or older
+
+
+let age = 41
+let isChild = age <= 7
+let isSenior = age >= 65
+console.log('\r\n isChild & isSenior')
+console.log(isChild)
+console.log(isSenior)
+
+
+let age1 = 1
+let isChild1 = age1 <= 7
+let isSenior1 = age1 >= 65
+console.log('\r\n isChild')
+console.log(isChild1)
+console.log(isSenior1)
+
+
+let age2 = 65
+let isChild2 = age1 <= 7
+let isSenior2 = age1 >= 65
+console.log('\r\n isSenior')
+console.log(isChild2)
+console.log(isSenior2)
